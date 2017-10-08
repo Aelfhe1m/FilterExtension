34986d80a26f85b563195b5303be7e9c6a5b60bb
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-10-08 08:58:46 -0400

    Updated for 1.3.1

---
+++ deploy.bat
@@ -0,0 +1,17 @@
+
+@echo off
+
+set H=R:\KSP_1.3.1_dev
+set GAMEDIR=000_FilterExtensions
+set GAMEDIR2="000_FilterExtensions_Configs"
+
+echo %H%
+
+copy /Y "%1%2" "GameData\%GAMEDIR%\Plugins
+
+
+mkdir "%H%\GameData\%GAMEDIR%"
+xcopy  /E /y GameData\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+
+xcopy  /E /y /I  GameData\%GAMEDIR2% "%H%\GameData\%GAMEDIR2%"
+
